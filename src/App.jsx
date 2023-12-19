83a4c76cbb3338ba9639907e2f64d27ac65f810e
[name] <jaipal@createbytes.com>
2023-12-20 01:25:44 +0530

    created sidebar for the web

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
-import React from 'react'
-
-import './App.css'
-
-const internal = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-console.log(internal);
+import Sidebar from "./components/sidebar/Sidebar"
 
 function App() {
 
   return (
     <>
-      
-      <h1> Pokimon Application </h1>
-      <div className="card">
-          Edit <code>src/App.jsx</code> and save to test HMR
-      </div>
+      <Sidebar/>
     </>
   )
 }
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { BsHouseDoor } from "react-icons/bs";
+import { PiBathtub } from "react-icons/pi";
+import { GrTransaction } from "react-icons/gr";
+import { IoIosGitNetwork, IoMdOpen } from "react-icons/io";
+import { PiStackFill } from "react-icons/pi";
+import { IoPeople } from "react-icons/io5";
+import { CiBank } from "react-icons/ci";
+import { FaQuestion } from "react-icons/fa6";
+import { IoIosSettings } from "react-icons/io";
+import Logo from '../../assets/zintlr.png';
+import "./sidebar.css";
+
+export const Sidebar = () => {
+  const SideBarContent = [
+    {
+      tag: "Dashboard",
+      icon: <BsHouseDoor />,
+    },
+    {
+      icon: <PiBathtub />,
+      tag: "Bullion Operations",
+    },
+    {
+      icon: <GrTransaction />,
+      tag: "Transections",
+    },
+    {
+      icon: <IoIosGitNetwork />,
+      tag: "Your Network",
+    },
+    {
+      icon: <IoPeople />,
+      tag: "Consumers",
+    },
+    {
+      icon: <PiStackFill />,
+      tag: "Otner Operations",
+    },
+    {
+      icon: <CiBank />,
+      tag: "Manage Bank Accounts",
+    },
+    {
+      icon: <FaQuestion />,
+      tag: "FAQ",
+    },
+    {
+      icon: <IoIosSettings />,
+      tag: "Customize Dashboard",
+    },
+  ];
+
+  return (
+    <div className="box">
+        <div>
+            <img src={Logo} alt="logo image" />
+        </div>
+      {SideBarContent.map((ele) => {
+        return (
+          <div className="bar-ele" key={Math.random() * 10}>
+            <div>
+              <h1>{ele.icon}</h1>
+               <p>{ele.tag}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Sidebar;
