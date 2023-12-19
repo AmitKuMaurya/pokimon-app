2ffb9bca3e168f7940bf0229c18a6d60c7841c6d
[name] <jaipal@createbytes.com>
2023-12-20 04:27:39 +0530

    created navbar and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import HomePage from "./components/Home/HomePage"
 import Sidebar from "./components/sidebar/Sidebar"
-
+import "./App.css"
 function App() {
 
   return (
     <>
+    <div className="ocean">
       <Sidebar/>
+      <HomePage/>
+    </div>
     </>
   )
 }
--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Navbar } from '../navbar/Navbar';
-
+import "./home.css"
 const HomePage = () => {
   return (
-    <>
+    <div className='home'>
       <Navbar/>
-    </>
+    </div>
   )
 }
 
