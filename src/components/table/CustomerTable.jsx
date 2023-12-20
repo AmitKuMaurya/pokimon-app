b35ae449dbd95808052462431fe843249878d20b
[name] <jaipal@createbytes.com>
2023-12-20 18:56:40 +0530

    customer table is completed with customer navbar of filter search ad sort

--- src/components/table/CustomerTable.jsx
+++ src/components/table/CustomerTable.jsx
@@ -1,116 +1,188 @@
 import React from "react";
-import { Space, Table, Tag } from "antd";
+import { Space, Table, Tag, Select } from "antd";
 import { LiaArrowRightSolid } from "react-icons/lia";
-import "./customerTable.css"
+import "./customerTable.css";
 import { TableData } from "../../constants/table.data";
-
-
+import { CiSearch } from "react-icons/ci";
 
 const CustomerTable = () => {
-
-    const columns = [
-        {
-          title: 'Customer Name',
-          dataIndex: 'name',
-          key: 'name',
-          render: (text) => <a>{text}</a>,
-        },
-        {
-          title: 'Assets',
-          dataIndex: 'assets',
-          key: 'assets',
-        //   render : (text) => <p>{text.amount}</p>
-            render: (tags) => (
-            <>
-                  <p>{tags.amount}</p>
-              {tags.units.map((tag) => {
-                let color = tag.length > 5 ? 'geekblue' : 'green';
-                if (tag === 'loser') {
-                  color = 'volcano';
-                }
-                return (
-                    <>
-                  <Tag color={color} key={tag}>
-                    {tag.toLowerCase()}
-                  </Tag>
-                    </>
-                );
-              })}
-            </>
-          ),
-        },
-        {
-          title: 'Revenue',
-          dataIndex: 'revenue',
-          key: 'revenue',
-        },
-        {
-          title: 'KYC Status',
-          dataIndex: 'kyc_status',
-          key: 'kyc_status',
-          render : (status) =>{
-              {var color = status.length < 5 ? "green" : "red"
-              console.log('status: ', status.length);
-                if(status === "Pending"){
-                    color = "orange";
-                }
-                } 
+  const columns = [
+    {
+      title: "Customer Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Assets",
+      dataIndex: "assets",
+      key: "assets",
+      render: (tags) => (
+        <>
+          <p>{tags.amount}</p>
+          {tags.units.map((tag) => {
+            let color = tag.length > 5 ? "geekblue" : "green";
+            if (tag === "loser") {
+              color = "volcano";
+            }
             return (
-                <>
-                <Tag color={color}>
-                    {status}
+              <>
+                <Tag color={color} key={tag}>
+                  {tag.toLowerCase()}
                 </Tag>
-                </>
-            )
-          } 
-        },
+              </>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: "Revenue",
+      dataIndex: "revenue",
+      key: "revenue",
+    },
+    {
+      title: "KYC Status",
+      dataIndex: "kyc_status",
+      key: "kyc_status",
+      render: (status) => {
         {
-          title: 'Facilitator',
-          dataIndex: 'facilitator',
-          key: 'facilitator',
-          render : (ele)=> {
-            return <div className="facilitator">
-            <div>
-                <p>{ele.business.name}</p>
-                <p>{ele.business.user}</p>
-            </div>
-            <div>
-            <LiaArrowRightSolid />
-            </div>
-            <div>
-                <p>{ele.associate.name}</p>
-                <p>{ele.associate.user}</p>
-            </div>
-            </div>
+          var color = status.length < 5 ? "green" : "red";
+          console.log("status: ", status.length);
+          if (status === "Progress") {
+            color = "orange";
           }
-        },
-        {
-          title: 'Zone',
-          dataIndex: 'zone',
-          key: 'zone',
-        },
-        {
-          title: 'Account Type',
-          dataIndex: 'account_type',
-          key: 'account_type',
-        },
-        {
-          title: 'TPA',
-          key: 'tpa',
-          dataIndex: 'tpa',
-        },
-        // {
-        //   title: 'Action',
-        //   key: 'action',
-        //   render: (_, record) => (
-        //     <Space size="middle">
-        //       <a>Invite {record.name}</a>
-        //       <a>Delete</a>
-        //     </Space>
-        //   ),
-        // },
-      ];
+        }
+        return (
+          <>
+            <Tag color={color}>{status}</Tag>
+          </>
+        );
+      },
+    },
+    {
+      title: "Facilitator",
+      dataIndex: "facilitator",
+      key: "facilitator",
+      render: (ele) => {
+        return (
+          <div className="facilitator">
+            <div>
+              <p>{ele.business.name}</p>
+              <p>{ele.business.user}</p>
+            </div>
+            <div>
+              <LiaArrowRightSolid />
+            </div>
+            <div>
+              <p>{ele.associate.name}</p>
+              <p>{ele.associate.user}</p>
+            </div>
+          </div>
+        );
+      },
+    },
+    {
+      title: "Zone",
+      dataIndex: "zone",
+      key: "zone",
+    },
+    {
+      title: "Account Type",
+      dataIndex: "account_type",
+      key: "account_type",
+    },
+    {
+      title: "TPA",
+      key: "tpa",
+      dataIndex: "tpa",
+    },
+  ];
 
-  return <Table columns={columns} dataSource={TableData} />;
+  const onChange = (value) => {
+    console.log(`selected ${value}`);
+  };
+  //   const onSearch = (value) => {
+  //     console.log('search:', value);
+  //   };
+
+  // Filter `option.label` match the user type `input`
+  const filterOption = (input, option) =>
+    (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
+
+  return (
+    <>
+      <div className="heading">Consumers Overview</div>
+      <div className="customer-nav">
+        <div>
+          <input type="text" name="" id="" placeholder="Search Ex:(Rewards)" />
+          <h1>
+            <CiSearch />
+          </h1>
+        </div>
+        <div>
+          {/* sort, filter, reset */}
+          <Select
+            showSearch
+            placeholder="Filter By Zone or KYC Status"
+            // optionFilterProp="children"
+            onChange={onChange}
+            // onSearch={onSearch}
+            filterOption={filterOption}
+            options={[
+              {
+                value: "east",
+                label: "Filter By East Zone",
+              },
+              {
+                value: "west",
+                label: "Filter By West Zone",
+              },
+              {
+                value: "north",
+                label: "Filter By North Zone",
+              },
+              {
+                value: "south",
+                label: "Filter By South Zone",
+              },
+            ]}
+          />
+
+          <Select
+            showSearch
+            placeholder="Sort By Name or Revenue"
+            // optionFilterProp="children"
+            onChange={onChange}
+            // onSearch={onSearch}
+            filterOption={filterOption}
+            options={[
+              {
+                value: "name",
+                label: "Sort By Name A - Z",
+              },
+              {
+                value: "name",
+                label: "Sort By Name Z- A",
+              },
+              {
+                value: "asc",
+                label: "Sort By Revenue ASC",
+              },
+              {
+                value: "desc",
+                label: "Sort By Revenue DESC",
+              },
+            ]}
+          />
+        </div>
+      </div>
+      <Table
+        columns={columns}
+        pagination={{ pageSize: 4 }}
+        dataSource={TableData}
+      />
+    </>
+  );
 };
 export default CustomerTable;
--- src/constants/table.data.js
+++ src/constants/table.data.js
@@ -1,182 +1,315 @@
 export const TableData = [
-    {
-      name: "John Doe",
-      assets: {
-        amount: "₹25,678",
-        units: ["30.5g", "20.2g", "₹150.75"],
-      },
-      revenue: "₹25,678",
-      kyc_status: "Done",
-      facilitator: {
-        business: {
-          name: "B2B Field Executive",
-          user: "Michael Smith",
-        },
-        associate: {
-          name: "Associate",
-          user: "Emily Johnson",
-        },
-      },
-      zone: "North",
-      account_type: "Pay and User (Major)",
-      tpa: "9876543210@trucard",
+  {
+    name: "John Doe",
+    assets: {
+      amount: "₹25,678",
+      units: ["30.5g", "20.2g", "₹150.75"],
     },
-    {
-      name: "Jane Smith",
-      assets: {
-        amount: "₹40,123",
-        units: ["55.2g", "40.8g", "₹300.50"],
+    revenue: "₹25,678",
+    kyc_status: "Done",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Michael Smith",
       },
-      revenue: "₹40,123",
-      kyc_status: "Pending",
-      facilitator: {
-        business: {
-          name: "B2B Field Executive",
-          user: "Chris Williams",
-        },
-        associate: {
-          name: "Associate",
-          user: "Olivia Davis",
-        },
+      associate: {
+        name: "Associate",
+        user: "Emily Johnson",
       },
-      zone: "East",
-      account_type: "Pay and User (Minor)",
-      tpa: "9876123450@trucard",
     },
-    {
-      name: "Eva Johnson",
-      assets: {
-        amount: "₹18,765",
-        units: ["25.1g", "18.7g", "₹120.25"],
-      },
-      revenue: "₹18,765",
-      kyc_status: "Done",
-      facilitator: {
-        business: {
-          name: "B2B Field Executive",
-          user: "Daniel Miller",
-        },
-        associate: {
-          name: "Associate",
-          user: "Sophia Brown",
-        },
-      },
-      zone: "West",
-      account_type: "Pay and User (Major)",
-      tpa: "9876549876@trucard",
+    zone: "North",
+    account_type: "Pay and User (Major)",
+    tpa: "9876543210@trucard",
+  },
+  {
+    name: "Jane Smith",
+    assets: {
+      amount: "₹40,123",
+      units: ["55.2g", "40.8g", "₹300.50"],
     },
-    {
-      name: "William Turner",
-      assets: {
-        amount: "₹30,987",
-        units: ["45.5g", "30.1g", "₹210.80"],
+    revenue: "₹40,123",
+    kyc_status: "Progress",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Chris Williams",
       },
-      revenue: "₹30,987",
-      kyc_status: "Pending",
-      facilitator: {
-        business: {
-          name: "B2B Field Executive",
-          user: "Emma Davis",
-        },
-        associate: {
-          name: "Associate",
-          user: "Liam Wilson",
-        },
+      associate: {
+        name: "Associate",
+        user: "Olivia Davis",
       },
-      zone: "South",
-      account_type: "Pay and User (Minor)",
-      tpa: "9876123450@trucard",
     },
+    zone: "East",
+    account_type: "Pay and User (Minor)",
+    tpa: "9876123450@trucard",
+  },
+  {
+    name: "Eva Johnson",
+    assets: {
+      amount: "₹18,765",
+      units: ["25.1g", "18.7g", "₹120.25"],
+    },
+    revenue: "₹18,765",
+    kyc_status: "Done",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Daniel Miller",
+      },
+      associate: {
+        name: "Associate",
+        user: "Sophia Brown",
+      },
+    },
+    zone: "West",
+    account_type: "Pay and User (Major)",
+    tpa: "9876549876@trucard",
+  },
+  {
+    name: "William Turner",
+    assets: {
+      amount: "₹30,987",
+      units: ["45.5g", "30.1g", "₹210.80"],
+    },
+    revenue: "₹30,987",
+    kyc_status: "Pending",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Emma Davis",
+      },
+      associate: {
+        name: "Associate",
+        user: "Liam Wilson",
+      },
+    },
+    zone: "South",
+    account_type: "Pay and User (Minor)",
+    tpa: "9876123450@trucard",
+  },
+
+  {
+    name: "Mia Brown",
+    assets: {
+      amount: "₹29,870",
+      units: ["43.7g", "29.1g", "₹205.40"],
+    },
+    revenue: "₹29,870",
+    kyc_status: "Progress",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Noah Johnson",
+      },
+      associate: {
+        name: "Associate",
+        user: "Emma Davis",
+      },
+    },
+    zone: "North",
+    account_type: "Pay and User (Major)",
+    tpa: "9876548765@trucard",
+  },
+  {
+    name: "Ethan White",
+    assets: {
+      amount: "₹24,320",
+      units: ["35.4g", "24.8g", "₹155.80"],
+    },
+    revenue: "₹24,320",
+    kyc_status: "Done",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Ava Miller",
+      },
+      associate: {
+        name: "Associate",
+        user: "Logan Wilson",
+      },
+    },
+    zone: "East",
+    account_type: "Pay and User (Minor)",
+    tpa: "9876123578@trucard",
+  },
+
+  {
+    name: "Sophie Miller",
+    assets: {
+      amount: "₹22,450",
+      units: ["32.8g", "22.1g", "₹165.40"],
+    },
+    revenue: "₹22,450",
+    kyc_status: "Progress",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Noah Brown",
+      },
+      associate: {
+        name: "Associate",
+        user: "Mia Smith",
+      },
+    },
+    zone: "North",
+    account_type: "Pay and User (Major)",
+    tpa: "9876548901@trucard",
+  },
+  {
+    name: "Liam Davis",
+    assets: {
+      amount: "₹28,890",
+      units: ["40.2g", "28.7g", "₹198.75"],
+    },
+    revenue: "₹28,890",
+    kyc_status: "Done",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Ava Johnson",
+      },
+      associate: {
+        name: "Associate",
+        user: "Logan White",
+      },
+    },
+    zone: "East",
+    account_type: "Pay and User (Minor)",
+    tpa: "9876123490@trucard",
+  },
+
+  {
+    name: "Oliver Smith",
+    assets: {
+      amount: "₹33,210",
+      units: ["48.6g", "33.2g", "₹225.90"],
+    },
+    revenue: "₹33,210",
+    kyc_status: "Done",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Isabella Davis",
+      },
+      associate: {
+        name: "Associate",
+        user: "Ethan Wilson",
+      },
+    },
+    zone: "West",
+    account_type: "Pay and User (Major)",
+    tpa: "9876547654@trucard",
+  },
+  {
+    name: "Ava Wilson",
+    assets: {
+      amount: "₹26,550",
+      units: ["38.9g", "26.3g", "₹180.60"],
+    },
+    revenue: "₹26,550",
+    kyc_status: "Pending",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Lucas Miller",
+      },
+      associate: {
+        name: "Associate",
+        user: "Sophia White",
+      },
+    },
+    zone: "South",
+    account_type: "Pay and User (Minor)",
+    tpa: "9876123789@trucard",
+  },
+
+  {
+    name: "Sophia Davis",
+    assets: {
+      amount: "₹31,490",
+      units: ["46.2g", "31.8g", "₹215.30"],
+    },
+    revenue: "₹31,490",
+    kyc_status: "Pending",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Lucas Smith",
+      },
+      associate: {
+        name: "Associate",
+        user: "Eva Wilson",
+      },
+    },
+    zone: "West",
+    account_type: "Pay and User (Major)",
+    tpa: "9876547890@trucard",
+  },
+  {
+    name: "Logan Johnson",
+    assets: {
+      amount: "₹27,810",
+      units: ["40.6g", "27.2g", "₹192.15"],
+    },
+    revenue: "₹27,810",
+    kyc_status: "Done",
+    facilitator: {
+      business: {
+        name: "B2B Field Executive",
+        user: "Isaac Davis",
+      },
+      associate: {
+        name: "Associate",
+        user: "Ava Wilson",
+      },
+    },
+    zone: "South",
+    account_type: "Pay and User (Minor)",
+    tpa: "9876123456@trucard",
+  },
 
 {
-  name: "Sophie Miller",
+  name: "Noah Wilson",
   assets: {
-    amount: "₹22,450",
-    units: ["32.8g", "22.1g", "₹165.40"],
+    amount: "₹23,670",
+    units: ["34.5g", "23.9g", "₹168.20"],
   },
-  revenue: "₹22,450",
-  kyc_status: "Process",
+  revenue: "₹23,670",
+  kyc_status: "Progress",
   facilitator: {
     business: {
       name: "B2B Field Executive",
-      user: "Noah Brown",
+      user: "Mia White",
     },
     associate: {
       name: "Associate",
-      user: "Mia Smith",
+      user: "Liam Davis",
     },
   },
   zone: "North",
   account_type: "Pay and User (Major)",
-  tpa: "9876548901@trucard",
+  tpa: "9876548765@trucard",
 },
 {
-  name: "Liam Davis",
+  name: "Ava Miller",
   assets: {
-    amount: "₹28,890",
-    units: ["40.2g", "28.7g", "₹198.75"],
+    amount: "₹26,340",
+    units: ["38.4g", "26.8g", "₹187.50"],
   },
-  revenue: "₹28,890",
+  revenue: "₹26,340",
   kyc_status: "Done",
   facilitator: {
     business: {
       name: "B2B Field Executive",
-      user: "Ava Johnson",
+      user: "Ethan Wilson",
     },
     associate: {
       name: "Associate",
-      user: "Logan White",
+      user: "Sophia Smith",
     },
   },
   zone: "East",
   account_type: "Pay and User (Minor)",
-  tpa: "9876123490@trucard",
+  tpa: "9876123456@trucard",
 },
-
-{
-  name: "Oliver Smith",
-  assets: {
-    amount: "₹33,210",
-    units: ["48.6g", "33.2g", "₹225.90"],
-  },
-  revenue: "₹33,210",
-  kyc_status: "Done",
-  facilitator: {
-    business: {
-      name: "B2B Field Executive",
-      user: "Isabella Davis",
-    },
-    associate: {
-      name: "Associate",
-      user: "Ethan Wilson",
-    },
-  },
-  zone: "West",
-  account_type: "Pay and User (Major)",
-  tpa: "9876547654@trucard",
-},
-{
-  name: "Ava Wilson",
-  assets: {
-    amount: "₹26,550",
-    units: ["38.9g", "26.3g", "₹180.60"],
-  },
-  revenue: "₹26,550",
-  kyc_status: "Pending",
-  facilitator: {
-    business: {
-      name: "B2B Field Executive",
-      user: "Lucas Miller",
-    },
-    associate: {
-      name: "Associate",
-      user: "Sophia White",
-    },
-  },
-  zone: "South",
-  account_type: "Pay and User (Minor)",
-  tpa: "9876123789@trucard",
-},
-
-    
-  ];
+];
